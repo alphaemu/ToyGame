e5d998ee4862e716af6ed97c19b5f9dd3ecb966e
Hman26 <52834844+Hman26@users.noreply.github.com>
2022-07-28 16:52:38 -0400

    Improved dialogue system
    Allows for multiple characters dialogue and to deactive the dialogue when the character leaves

--- ToyGame/Assets/Scripts/DialougeSystem.cs
+++ ToyGame/Assets/Scripts/DialougeSystem.cs
@@ -6,13 +6,17 @@ using UnityEngine.UI;
 public class DialougeSystem : MonoBehaviour
 {
     public Text dialogue1;
+    public Text dialogue2; 
 
     private bool isDialougeChecked;
+    private bool character1;
+    private bool character2; 
 
     // Start is called before the first frame update
     void Start()
     {
         dialogue1.gameObject.SetActive(false);
+        dialogue2.gameObject.SetActive(false);
         isDialougeChecked = false;
     }
 
@@ -21,20 +25,68 @@ public class DialougeSystem : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E) && isDialougeChecked == true)
         {
-            dialogue1.gameObject.SetActive(true);
-            isDialougeChecked = false;
+            if (character1 == true)
+            {
+                dialogue1.gameObject.SetActive(true);
+                isDialougeChecked = false;
+            }
+
+            if(character2 == true)
+            {
+                dialogue2.gameObject.SetActive(true);
+                isDialougeChecked = false;
+            }
+
         }
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "NPC")
+        if (other.gameObject.tag == "Character1")
         {
             Debug.Log("Press E to start dialouge");
             isDialougeChecked = true;
+            character1 = true; 
+
+
+        }
+
+        if (other.gameObject.tag == "Character2")
+        {
+            Debug.Log("Press E to start dialouge");
+            isDialougeChecked = true;
+            character2 = true;
 
 
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.tag == "Character1")
+        {
+            //Debug.Log("Press E to start dialouge");
+            dialogue1.gameObject.SetActive(false);
+            isDialougeChecked = false;
+            character1 = false;
+
+
+        }
+
+        if (other.gameObject.tag == "Character2")
+        {
+            //Debug.Log("Press E to start dialouge");
+            dialogue2.gameObject.SetActive(false);
+            isDialougeChecked = false;
+            character2 = false;
+
+
+        }
+    }
+
+
+
+
 }
