83fe6124c3f67ffd5dfa1b6895a2e0a276c42304
E <eberlin.pro@gmail.com>
2022-08-18 10:51:01 -0400

    Pre-Thursday Push
    Added thing that appears to notify you that you can interact with characters

--- ToyGame/Assets/Scripts/DialougeSystem.cs
+++ ToyGame/Assets/Scripts/DialougeSystem.cs
@@ -19,6 +19,15 @@ public class DialougeSystem : MonoBehaviour
     // Gremlin Dialogues 
     public GameObject gremlinDialogue;
 
+    // Interaction popups
+    public GameObject notifBear1;
+    public GameObject notifBear2;
+    public GameObject notifBear3;
+    public GameObject notifPenguin1;
+    public GameObject notifPenguin2;
+    public GameObject notifGremlin;
+
+    // Player Location
 
     private bool isDialougeChecked;
     private bool character1;
@@ -41,6 +50,7 @@ public class DialougeSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Dialogue = Invisible
         teddyBearDialogue1.SetActive(false);
         teddyBearDialogue1Part2.SetActive(false); 
         teddyBearDialogue2.SetActive(false);
@@ -48,12 +58,22 @@ public class DialougeSystem : MonoBehaviour
         penguinDialogue1.SetActive(false);
         penguinDialogue2.SetActive(false);
         gremlinDialogue.SetActive(false);
+
+        // Popups = Invisible
+        notifBear1.SetActive(false);
+        notifBear2.SetActive(false);
+        notifBear3.SetActive(false);
+        notifPenguin1.SetActive(false);
+        notifPenguin2.SetActive(false);
+        notifGremlin.SetActive(false);
+
         isDialougeChecked = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.E) && isDialougeChecked == true)
         {
             if (teddyBear1 == true)
@@ -110,6 +130,7 @@ public class DialougeSystem : MonoBehaviour
             Debug.Log("Press E to start dialouge");
             isDialougeChecked = true;
             teddyBear1 = true;
+            notifBear1.SetActive(true);
         }
 
         if (other.gameObject.tag == "TeddyBear1Part2")
@@ -124,6 +145,7 @@ public class DialougeSystem : MonoBehaviour
             Debug.Log("Press E to start dialouge");
             isDialougeChecked = true;
             teddyBear2 = true;
+            notifBear2.SetActive(true);
         }
 
         if (other.gameObject.tag == "TeddyBear3")
@@ -131,6 +153,7 @@ public class DialougeSystem : MonoBehaviour
             Debug.Log("Press E to start dialouge");
             isDialougeChecked = true;
             teddyBear3 = true;
+            notifBear3.SetActive(true);
         }
 
 
@@ -139,6 +162,7 @@ public class DialougeSystem : MonoBehaviour
             Debug.Log("Press E to start dialouge");
             isDialougeChecked = true;
             penguin1 = true;
+            notifPenguin1.SetActive(true);
         }
 
         if (other.gameObject.tag == "Penguin2")
@@ -146,6 +170,7 @@ public class DialougeSystem : MonoBehaviour
             Debug.Log("Press E to start dialouge");
             isDialougeChecked = true;
             penguin2 = true;
+            notifPenguin2.SetActive(true);
 
 
         }
@@ -155,6 +180,7 @@ public class DialougeSystem : MonoBehaviour
             Debug.Log("Press E to start dialouge");
             isDialougeChecked = true;
             gremlin = true;
+            notifGremlin.SetActive(true);
         }
     }
 
@@ -170,6 +196,7 @@ public class DialougeSystem : MonoBehaviour
             teddyBearDialogue1.SetActive(false);
             isDialougeChecked = false;
             teddyBear1 = false;
+            notifBear1.SetActive(false);
         }
 
         if (other.gameObject.tag == "TeddyBear1Part2")
@@ -182,18 +209,18 @@ public class DialougeSystem : MonoBehaviour
 
         if (other.gameObject.tag == "TeddyBear2")
         {
-
             teddyBearDialogue2.SetActive(false);
             isDialougeChecked = false;
             teddyBear2 = false;
+            notifBear2.SetActive(false);
         }
 
         if (other.gameObject.tag == "TeddyBear3")
         {
-
             teddyBearDialogue3.SetActive(false);
             isDialougeChecked = false;
             teddyBear3 = false;
+            notifBear3.SetActive(false);
         }
 
         if (other.gameObject.tag == "Penguin1")
@@ -201,26 +228,23 @@ public class DialougeSystem : MonoBehaviour
             penguinDialogue1.SetActive(false);
             isDialougeChecked = false;
             penguin1 = false;
-
+            notifPenguin1.SetActive(false);
         }
 
         if (other.gameObject.tag == "Penguin2")
         {
-
             penguinDialogue2.SetActive(false);
             isDialougeChecked = false;
             penguin2 = false;
-
+            notifPenguin2.SetActive(false);
         }
 
         if (other.gameObject.tag == "Gremlin")
         {
-
-
             gremlinDialogue.SetActive(false);
             isDialougeChecked = false;
             gremlin = false;
-
+            notifGremlin.SetActive(false);
         }
 
     }
